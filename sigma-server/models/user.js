2a633bf9c91d148038cb86e381076e29897ead20
KIM\joo <qweasd_147@naver.com>
2018-10-26 18:02:21 +0900

    암호화 모듈 설치 및 사용자 모델 수정

--- sigma-server/models/user.js
+++ sigma-server/models/user.js
@@ -1,6 +1,15 @@
 const Sequelize = require('sequelize');
 const db = require('./db');
 
+const crypto = require('crypto')
+
+const USER_STATUS = {
+    "ENABLE" : "E"
+    , "LOCK" : "L"
+    , "EXPIRED" : "EX"
+}
+
+
 //TODO : 컬럼 & 컬럼명 조정
 const User = db.define('User', {
         id: {
@@ -17,17 +26,76 @@ const User = db.define('User', {
             , allowNull : false
         }
         , status : {
-            type : Sequelize.TEXT
+            type : Sequelize.ENUM(Object.values(USER_STATUS))
             , allowNull : false
+        }
+        , create : {
+            type: Sequelize.DATE
+            , default: Sequelize.NOW,
         }
     }
     ,{
         timestamps: false
         , tableName: 'User'
+    }
+);
+
+User.prototype.encode = (str) => {
+    return crypto.createHash('sha256')
+    .update(str)
+    .digest('base64');
+};
+
+User.isEqualsPasswd = (pw) => {
+
+    const inputPw = this.encode(pwd);
+
+    return this.passwd === inputPw;
+};
+
+User.register = async(pw, email, status) => {
+    
+    const user = await User.build({
+        passwd : this.encode(pw)
+        , email
+        , status
+    }).save();
+    
+    return user;
+};
+
+/**
+ * 이메일 중복값 체크.
+ * 중복 된 이메일이 있을 시 true
+ * 없을 시 false
+ */
+User.isEnableEmail = async(email) => {
+    const user = await Post.findOne({
+        where: {
+            email
+        }
     });
-  
-  User.serialize = (data) => {
-    return data.toJSON();
-  };
-  
-  module.exports = User;
+
+    if(user)    return true;
+    else        return false;
+}
+
+User.findUserForLogin = async(email, pw) => {
+    const user = await Post.findOne({
+        where: {
+            email
+        }
+    });
+
+    if(user && this.encode(pw) === user.passwd)
+        return user;
+    else
+        return null;
+}
+
+User.serialize = () => {
+    return this.toJSON();
+};
+
+module.exports = User;
+//module.exports = USER_STATUS;
